089f910ef46be84668ff699f2880d977fc2c751f
Agustin Landa <93983553+Landa86@users.noreply.github.com>
2024-07-03 13:58:02 -0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,31 +1,28 @@
-from flask import Blueprint, render_template, redirect, url_for
-from controllers.user import add_user_function,edit_user_function,delete_user_function  
-import sys
-from models.user import User 
+from flask import Flask,render_template
+from config import Config
+from extensions import db
+from flask_migrate import Migrate
+from routes import main
 
 
-main = Blueprint("main",__name__)
 
-@main.route("/",methods=["GET"])
-def home():
-    data = User.get_all()
-    return render_template("index.html",data=data)
+def create_app():
+    app = Flask (__name__)
+    app.config.from_object(Config)
+    extensions(app) 
+    register_resources(app)
+    return app
 
-@main.route("/adduser", methods=["GET","POST"])
-def add_user_route():
-    data = add_user_function()
-    print(data,file=sys.stderr)
-    return render_template("adduser.html",data=data)
+def extensions(app):
+        db.init_app(app)
+        migrate = Migrate(app,db)
 
-@main.route("/edituser/<int:id>", methods=["GET","POST"])
-def edit_user(id):
-    user = User.get_by_id(id)
-    data = edit_user_function(user)
-    return render_template("edituser.html", user=user, data=data)
 
-@main.route("/deleteuser/<int:id>", methods=["POST"])
-def delete_user(id):
-    user = User.get_by_id(id)
-    print(user,file=sys.stderr)
-    delete_user_function(user)
-    return redirect(url_for("main,home"))
+def register_resources(app):
+    app.register_blueprint(main)
+    
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
+
